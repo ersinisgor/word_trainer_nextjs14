bea901e406f24ddbe1bfe605264955616e11d309
Ersin Isgor <ersinisgor@hotmail.com>
2024-07-14 17:41:20 +0300

    refactor: Normalize and sanitize fields in VocabularySchema pre-save middleware
    This commit refactors the pre-save middleware in the VocabularySchema to improve the normalization and sanitization of fields. The middleware now uses helper functions to remove extra spaces, trim strings, normalize text, and capitalize the first letter of sentences. It also applies these functions to various fields such as word, turkishMeanings, sideNotes, englishExpression, exampleSentences, imageUrl, type, and tags. These changes enhance the data consistency and readability of the vocabulary entries.
    
    Note: This commit message follows the conventional format of starting with a type (refactor) followed by a brief description of the changes made.

--- lib/models/Vocabulary.ts
+++ lib/models/Vocabulary.ts
@@ -33,20 +33,38 @@ const VocabularySchema = new Schema<IVocabulary>(
   { timestamps: true }
 );
 
-// Pre-save middleware to convert fields to lowercase
+// Function to remove extra spaces and trim. Replaces multiple spaces with a single space and trims the string
+const cleanWhitespace = (str: string) => str.replace(/\s+/g, " ").trim();
+
+// Function to normalize text before capitalizing the first letter
+const normalizeText = (sentence: string) =>
+  cleanWhitespace(sentence.toLowerCase());
+
+// Function to capitalize the first letter of a sentence
+const capitalizeFirstLetter = (sentence: string) => {
+  const cleanedSentence = cleanWhitespace(sentence);
+  return cleanedSentence.charAt(0).toUpperCase() + cleanedSentence.slice(1);
+};
+
+// Helper function to sanitize and normalize an array of strings
+const sanitizeAndNormalizeArray = (arr: string[]) => arr.map(normalizeText);
+
+// Pre-save middleware to process fields
 VocabularySchema.pre("save", function (next) {
-  this.word = this.word.toLowerCase();
-  this.meanings.turkishMeanings = this.meanings.turkishMeanings.map(str =>
-    str.toLowerCase()
+  this.word = cleanWhitespace(this.word.toLowerCase());
+  this.meanings.turkishMeanings = sanitizeAndNormalizeArray(
+    this.meanings.turkishMeanings
   );
-  this.meanings.sideNotes = this.meanings.sideNotes.map(str =>
-    str.toLowerCase()
+  this.meanings.sideNotes = sanitizeAndNormalizeArray(this.meanings.sideNotes);
+  this.englishExpression = capitalizeFirstLetter(
+    normalizeText(this.englishExpression)
   );
-  this.englishExpression = this.englishExpression.toLowerCase();
-  this.exampleSentences = this.exampleSentences.map(str => str.toLowerCase());
-  this.imageUrl = this.imageUrl ? this.imageUrl.toLowerCase() : "";
-  this.type = this.type.toLowerCase();
-  this.tags = this.tags.map(str => str.toLowerCase());
+  this.exampleSentences = this.exampleSentences.map(sentence =>
+    capitalizeFirstLetter(normalizeText(sentence))
+  );
+  this.imageUrl = this.imageUrl ? normalizeText(this.imageUrl) : "";
+  this.type = cleanWhitespace(this.type.toLowerCase());
+  this.tags = sanitizeAndNormalizeArray(this.tags);
   next();
 });
 
