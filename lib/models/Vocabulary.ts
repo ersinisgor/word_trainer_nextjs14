f886f30b8d3e5860d64d98cb84580f8cd7f85a1d
Ersin Isgor <ersinisgor@hotmail.com>
2024-07-15 11:20:20 +0300

    refactor: Add cloze sentence generation in VocabularySchema pre-save middleware

--- lib/models/Vocabulary.ts
+++ lib/models/Vocabulary.ts
@@ -10,6 +10,7 @@ interface IVocabulary extends Document {
   };
   englishExpression: string;
   exampleSentences: string[];
+  clozeSentences: { original: string; cloze: string }[];
   imageUrl?: string;
   type: string;
   tags: string[];
@@ -26,6 +27,7 @@ const VocabularySchema = new Schema<IVocabulary>(
     },
     englishExpression: String,
     exampleSentences: [String],
+    clozeSentences: [{ original: String, cloze: String }],
     imageUrl: String,
     type: String,
     tags: [String],
@@ -49,8 +51,29 @@ const capitalizeFirstLetter = (sentence: string) => {
 // Helper function to sanitize and normalize an array of strings
 const sanitizeAndNormalizeArray = (arr: string[]) => arr.map(normalizeText);
 
+// Function to generate cloze-deleted sentence
+const generateClozeSentence = (word: string, sentence: string) => {
+  const underscore = "_".repeat(word.length);
+  return sentence.replace(new RegExp(word, "gi"), underscore);
+};
+
+//new RegExp(word, "gi"):
+
+//RegExp is a built-in object in JavaScript used for matching patterns in strings.
+//It creates a regular expression object based on the word and the flags "gi":
+//"g" stands for global match, meaning it finds all matches rather than stopping after the first match.
+//"i" stands for case-insensitive match, meaning it ignores case distinctions when matching.
+
+//sentence.replace(...):
+
+//This method is called on the sentence string.
+//It searches for matches of the regular expression (RegExp) created earlier (new RegExp(word, "gi")).
+//When a match is found (where word occurs in sentence):
+//It replaces each match with the string specified by underscore
+
 // Pre-save middleware to process fields
 VocabularySchema.pre("save", function (next) {
+  // 'this' refers to the document being saved
   this.word = cleanWhitespace(this.word.toLowerCase());
   this.meanings.turkishMeanings = sanitizeAndNormalizeArray(
     this.meanings.turkishMeanings
@@ -62,6 +85,12 @@ VocabularySchema.pre("save", function (next) {
   this.exampleSentences = this.exampleSentences.map(sentence =>
     capitalizeFirstLetter(normalizeText(sentence))
   );
+
+  const wordToStudy = this.word;
+  this.clozeSentences = this.exampleSentences.map(sentence => ({
+    original: sentence,
+    cloze: generateClozeSentence(wordToStudy, sentence),
+  }));
   this.imageUrl = this.imageUrl ? normalizeText(this.imageUrl) : "";
   this.type = cleanWhitespace(this.type.toLowerCase());
   this.tags = sanitizeAndNormalizeArray(this.tags);
